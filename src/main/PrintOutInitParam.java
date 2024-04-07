fce5108006fab94d7417c342e0a9d17bda3088f3
Aravinth <aravinthbca2001@gmail.com>
2024-04-07 03:14:15 -0400

    Used ServletConfig to extract InitParam properties

---
+++ src/main/PrintOutInitParam.java
@@ -0,0 +1,31 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PrintOutInitParam extends HttpServlet {
+    
+    private static final Logger logger = LoggerFactory.getLogger(PrintOutInitParam.class);
+    ServletConfig config;
+
+    public void init(ServletConfig config){
+        this.config = config;
+    }
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.info("Entered service method");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Init Param Values : "+ config.getInitParameter("name2") +"</h1>");
+        out.println("</body></html>");
+        logger.info("Exited service method");
+    }
+
+}
