f71ae942e44a97947a35d600940e68a62d95c883
Aravinth <aravinthbca2001@gmail.com>
2024-04-07 05:26:59 -0400

    Creating Servlet Object for each request

---
+++ src/main/CustomServletFactory.java
@@ -0,0 +1,25 @@
+import jakarta.servlet.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class CustomServletFactory extends GenericServlet {
+
+    public static final Logger logger = LoggerFactory.getLogger(CustomServletFactory.class);
+
+    public CustomServletFactory(){
+        logger.info("Entered constructor method");
+        logger.info("Exited constructor method");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        logger.info("Entered service method");
+        // Create a new instance of your custom servlet here
+        TestingServletObjectCreation testingServletObjectCreation = new TestingServletObjectCreation();
+        testingServletObjectCreation.service(servletRequest, servletResponse);
+        logger.info("Exited service method");
+    }
+
+}
